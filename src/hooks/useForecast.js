6ab463b40cbc9edd912ec4d7ca4c0d4989e83e98
Guillermo <grlara9@gmail.com>
2022-01-16 16:55:29 -0600

    once we get id from location we use it to make api call to get weather data from user location

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -16,6 +16,14 @@ const useForecast = () =>{
         })
 
         console.log("ID", {data})
+
+        if(!data || data.length === 0){
+            setError("Location Not Found")
+            return
+        }
+
+        const promise = await axios(`${BASE_URL}/${data[0].woeid}`)
+        console.log("It worked", promise)
     }
     return{
         isError,
