2552f05af37c72ddc49d1e8438b548d8349b802f
Guillermo <grlara9@gmail.com>
2022-01-17 01:55:13 -0600

    separate two call in different function to organized code

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -8,7 +8,7 @@ const useForecast = () =>{
 
     const BASE_URL = 'https://the-ultimate-api-challenge.herokuapp.com/https://www.metaweather.com/api/location'
 
-    const submitRequest = async location =>{
+    const getWoeid = async location => {
         console.log("si es location", location)
         const {data} = await axios(`${BASE_URL}/search`, 
         {
@@ -21,10 +21,22 @@ const useForecast = () =>{
             setError("Location Not Found")
             return
         }
-
-        const promise = await axios(`${BASE_URL}/${data[0].woeid}`)
-        console.log("It worked", promise)
+    return data;
     }
+
+    const submitRequest = async woeid =>{
+     
+        const {data} = await axios(`${BASE_URL}/${woeid}`)
+       
+        if (!data || data.length === 0) {
+            setError('Something went wrong');
+            setLoading(false);
+            return;
+        }
+        return data
+    }
+
+
     return{
         isError,
         isLoading,
