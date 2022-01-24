3887dcad29b142074cc6664a6a90aec246b4d260
Guillermo <grlara9@gmail.com>
2022-01-23 21:16:25 -0600

    change backgroun color and kept styling current weather component

--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -1,19 +1,22 @@
 import React from 'react'
-
+import './CurrentDay.css'
 const CurrentDay = ({weekday, date, location, temperature, Description, weatherIcon}) => {
     return(
-        <div className={` d-flex flex-column justify-content-between pt-3 pb-2 pl-2`}>
+        <div className='current'>
             <div>
-                <p className="d-flex align-items-baseline font-weight-lighter mb-1"><span>{location}</span></p>
-                <h2 className="font-weight-bold mb-1">{weekday} | {date}</h2>
-                
+                <p><span>{location}</span></p>
+                <h2 className="font-weight-bold mb-1">{date}</h2>
             </div>
-            <div>
-                <img width="45" src={weatherIcon} alt="icon" />
-                <h2 className="font-weight-bold mb-1">
+            <div className='asd'>
+                <div className='icon'>
+                    <img width="80" src={weatherIcon} alt="icon" />
+                </div>
+                <div className='temperature'>
+                <h2>
                     <span>{temperature}</span>°C
                 </h2>
-                <h5 className="font-weight-lighter">{Description}</h5>
+                <h5>{Description}</h5>
+                </div>
             </div>
         </div>
    
--- src/helper/getCurrentDayForecast.js
+++ src/helper/getCurrentDayForecast.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Moment from 'react-moment';
 
 const getCurrentDayForecast =(data, title)=>({
-    weekday: data.applicable_date,
-    date: data.created,
+    weekday: <Moment>{data.applicable_date}</Moment>,
+    date: <Moment format="D MMM YYYY">{data.created}</Moment>,
     location: title,
     temperature: data.the_temp,
     weatherIcon:`https://www.metaweather.com/static/img/weather/${data.weather_state_abbr}.svg`,
