ae2ff3d4b02823deaffb954b5591ec0b955cb3c1
Guillermo <grlara9@gmail.com>
2022-02-03 23:40:42 -0600

    will implement toggler functionality so user can chose wether see temp in celsius or farenheit

---
+++ src/ui/Toggler.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Toggler = ( ) =>{
+return(
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+      </div>
+      <div>
+        <h1>Cool its {darkMode ? "Dark" : "Light"} Mode </h1>
+      </div>
+    </div>
+)
+
+}
+export default Toggler
